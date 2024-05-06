c846d440ab89ce3a375d4d0a8e9abe5f1b44321f
SuryayanMukhopadhyay <suryayanm7@gmail.com>
2024-05-06 21:13:32 +0530

    most busy users and wordcloud functions

--- helper.py
+++ helper.py
@@ -21,3 +21,36 @@ def fetch_stats(selected_user, df):
     for message in df['message']:
         links.extend(extract.find_urls(message))
     return num_messages, len(words), num_media_messages, len(links)
+
+
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head(10)
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x, df
+
+
+def create_wordcloud(selected_user, df):
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+
+    return df_wc
+
