61237363a6b73144654de38250ce708e49009d92
SuryayanMukhopadhyay <suryayanm7@gmail.com>
2024-05-06 21:12:50 +0530

    fetch_stats function added

---
+++ helper.py
@@ -0,0 +1,23 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji
+
+extract = URLExtract()
+
+
+def fetch_stats(selected_user, df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user] #slicing
+    num_messages = df.shape[0]
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0] #slicing
+
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+    return num_messages, len(words), num_media_messages, len(links)
