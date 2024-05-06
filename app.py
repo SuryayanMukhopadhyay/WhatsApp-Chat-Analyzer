340ae9807e8923fbae00ad61e7a859cef2f25a52
SuryayanMukhopadhyay <suryayanm7@gmail.com>
2024-05-06 21:17:00 +0530

    daily/ monthly timeline plotted

--- app.py
+++ app.py
@@ -10,3 +10,46 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
     df = preprocessor.preprocess(data)
+    
+    
+    # fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, "Overall")
+    selected_user = st.sidebar.selectbox("Show Analysis wrt", user_list)
+
+    if st.sidebar.button("Show Analysis"):
+        # Stats Area
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
+        st.title("Top Statistics")
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages:")
+            st.title(num_messages)
+        with col2:
+            st.header("Total Words:")
+            st.title(words)
+        with col3:
+            st.header("Media Shared:")
+            st.title(num_media_messages)
+        with col4:
+            st.header("Links Shared:")
+            st.title(num_links)
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
